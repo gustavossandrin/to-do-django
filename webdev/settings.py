0adc215a4ab2c491f7ad286470b0ccc31ed22c2e
Pichau <gustavosandrindattein123@gmail.com>
2022-07-12 07:33:42 -0300

    concertado allowed hosts

--- webdev/settings.py
+++ webdev/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-8s2x7ea_1i#py-dtmzd7c5u-pg%_)%jo4&rd22!-wou%*_y)i^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
